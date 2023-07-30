c5076958653bdff47dfa7ac462e00bbe60c6a59b
carreiras <ewertoncarreira@gmail.com>
2023-07-30 13:15:50 -0300

    Classe com muita responsabilidade (God Class)

---
+++ src/single_responsibility_principle/example_1/Main.java
@@ -0,0 +1,22 @@
+package single_responsibility_principle.example_1;
+
+public class Main {
+
+    public static void main(String[] args) {
+    }
+
+    public void listClient() {
+    }
+
+    public void addClient() {
+    }
+
+    public void updateClient() {
+    }
+
+    public void deleteClient() {
+    }
+
+    public void notifyClient() {
+    }
+}
