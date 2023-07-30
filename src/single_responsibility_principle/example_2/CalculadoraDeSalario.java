1b4c778f1197e1ed76564cf26ad7bc53226d3804
carreiras <ewertoncarreira@gmail.com>
2023-07-30 13:35:49 -0300

    Implementando interface para melhorar a coesão

--- src/single_responsibility_principle/example_2/CalculadoraDeSalario.java
+++ src/single_responsibility_principle/example_2/CalculadoraDeSalario.java
@@ -6,29 +6,13 @@ public class CalculadoraDeSalario {
 
     public double calcula(Funcionario funcionario) {
         if (DESENVOLVEDOR.equals(funcionario.getCargo())) {
-            return dezOuVintePorcento(funcionario);
+            return new DezOuVintePorCento().calcula(funcionario);
         }
 
         if (DBA.equals(funcionario.getCargo()) || TESTER.equals(funcionario.getCargo())) {
-            return quinzeOuVinteCincoPorcento(funcionario);
+            return new QuinzeOuVinteCincoPorCento().calcula(funcionario);
         }
 
         throw new RuntimeException("funcionario invalido");
-    }
-
-    private double dezOuVintePorcento(Funcionario funcionario) {
-        if (funcionario.getSalarioBase() > 3000.0) {
-            return funcionario.getSalarioBase() * 0.8;
-        } else {
-            return funcionario.getSalarioBase() * 0.9;
-        }
-    }
-
-    private double quinzeOuVinteCincoPorcento(Funcionario funcionario) {
-        if (funcionario.getSalarioBase() > 2000.0) {
-            return funcionario.getSalarioBase() * 0.75;
-        } else {
-            return funcionario.getSalarioBase() * 0.85;
-        }
     }
 }
---
+++ src/single_responsibility_principle/example_2/QuinzeOuVinteCincoPorCento.java
@@ -0,0 +1,13 @@
+package single_responsibility_principle.example_2;
+
+public class QuinzeOuVinteCincoPorCento implements RegraDeCalculo {
+
+    @Override
+    public double calcula(Funcionario funcionario) {
+        if (funcionario.getSalarioBase() > 2000.0) {
+            return funcionario.getSalarioBase() * 0.75;
+        } else {
+            return funcionario.getSalarioBase() * 0.85;
+        }
+    }
+}
