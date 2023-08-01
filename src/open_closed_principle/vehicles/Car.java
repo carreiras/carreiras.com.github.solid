dc190e26f28c611362e74428325ffdbab00832c7
carreiras <ewertoncarreira@gmail.com>
2023-07-31 21:47:24 -0300

    Utilizando OCP com interface

--- src/open_closed_principle/vehicles/Car.java
+++ src/open_closed_principle/vehicles/Car.java
@@ -1,22 +1,30 @@
 package open_closed_principle.vehicles;
 
 
-public class Car extends Vehicle {
+public class Car implements VehicleCar {
+
+    private String color;
+    private String year;
+    private double engine;
     private int seats;
 
     public Car(String color, String year, double engine, int seats) {
+        configureVehicle(color, year, engine, seats);
+    }
+
+    @Override
+    public void configureVehicle(String color, String year, double engine, int seats) {
         this.color = color;
         this.year = year;
         this.engine = engine;
         this.seats = seats;
 
-        configureCar();
+        System.out.println("Criando um carro com Interface: " + color + " " + engine);
+        startVehicle();
     }
 
-    public void configureCar() {
-        System.out.println("Criando um carro: " + color + " " +
-                year + " " + engine + " com " + seats + " assentos");
-
-        startVehicle();
+    @Override
+    public void startVehicle() {
+        System.out.println("Ligando os motores com interface");
     }
 }
---
+++ src/open_closed_principle/vehicles/VehicleCar.java
@@ -0,0 +1,8 @@
+package open_closed_principle.vehicles;
+
+public interface VehicleCar {
+
+    public void configureVehicle(String color, String year, double engine, int seats);
+
+    public void startVehicle();
+}
