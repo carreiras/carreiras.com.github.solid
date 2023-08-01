f6d80f142fb22b28f0e2b7a7aed96b9040a70bc7
carreiras <ewertoncarreira@gmail.com>
2023-07-31 21:37:57 -0300

    Utilizando OCP com herança

---
+++ src/open_closed_principle/vehicles/Motorcycle.java
@@ -0,0 +1,18 @@
+package open_closed_principle.vehicles;
+
+
+
+public class Motorcycle extends Vehicle {
+
+    public Motorcycle(String color, String year, double engine) {
+        this.color = color;
+        this.year = year;
+        this.engine = engine;
+
+        configureMotorcycle();
+    }
+
+    public void configureMotorcycle() {
+        System.out.println("Criando uma moto: " + year + " " + engine + " cilindradas");
+        startVehicle();
+    }}
---
+++ src/open_closed_principle/vehicles/Vehicle.java
@@ -0,0 +1,12 @@
+package open_closed_principle.vehicles;
+
+public class Vehicle {
+
+    protected String color;
+    protected String year;
+    protected double engine;
+
+    public void startVehicle() {
+        System.out.println("Ligando os motores");
+    }
+}
